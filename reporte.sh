d9f59951f2c7a1a20f93c172e73bb29fcb10aa60
Dennis-Hefziba <145822608+Dennis-Hefziba@users.noreply.github.com>
2024-04-08 01:08:41 +0000

    Codigo ya funcional

---
+++ reporte.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Ruta al archivo CSV
+archivo="/workspaces/Tarea-bloque-3/ventas.csv"
+
+# Redirigir toda la salida al archivo reporte.txt
+exec >> /workspaces/Tarea-bloque-3/reporte.txt
+
+# Generar el total de ventas por mes
+echo "Total de ventas por mes:"
+awk -F, '{split($1, fecha, "/"); mes = strftime("%B %Y", mktime("2000 " fecha[2] " " fecha[1] " 0 0 0")); ventas[mes] += $3} END {for (mes in ventas) print "Mes:", mes ", Total Ventas:", ventas[mes]}' $archivo
+
+echo ""
+
+# Producto más vendido en el año
+echo "Producto más vendido en el año:"
+awk -F, 'NR>1{productos[$2]+=$3} END {for (producto in productos) print productos[producto], producto}' $archivo | sort -nr | head -1 | while read monto producto; do
+    echo "Producto: $producto, Monto: $monto"
+done
+
+echo ""
+
+# Calcular el monto total anual
+total_anual=$(awk -F, 'NR>1{total+=$3} END {print total}' $archivo)
+echo "Monto total anual: $total_anual"
+
+echo ""
